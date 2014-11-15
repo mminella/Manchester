35f9601024fe772f3491310a6993eaee735b4362
Michael Minella <michael@michaelminella.com>
2014-11-14 23:37:55 -0600

    Add arrow indicator and calendar icon

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,3 +4,17 @@ $('.calendar-entry').each(function () {
     var tmpHeight = $(this).height();
     $(this).parent().height(tmpHeight);
 });
+
+$('#mainHeader a').each(function() {
+	$(this).hover(function() {
+		var element = $(this).parent().children('.arrow');
+		
+		element.toggleClass('arrow');
+		element.toggleClass('arrow_active');
+	}, function () {
+		var element = $(this).parent().children('.arrow_active');
+		
+		element.toggleClass('arrow');
+		element.toggleClass('arrow_active');
+	});	
+});
