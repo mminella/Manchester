e4c877e606a435d313da1fcf99078dede46cd220
Michael Minella <michael@michaelminella.com>
2014-11-15 17:51:47 -0600

    Completed basic home page styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,36 @@
 'use strict';
 
-$('.calendar-entry').each(function () {
-    var tmpHeight = $(this).height();
-    $(this).parent().height(tmpHeight);
-});
+$(document).ready(function() {
+	$('.calendar-entry').each(function () {
+	    var tmpHeight = $(this).height();
+	    $(this).parent().height(tmpHeight);
+	});
 
-$('#mainHeader a').each(function() {
-	$(this).hover(function() {
-		var element = $(this).parent().children('.arrow');
-		
-		element.toggleClass('arrow');
-		element.toggleClass('arrow_active');
-	}, function () {
-		var element = $(this).parent().children('.arrow_active');
-		
-		element.toggleClass('arrow');
-		element.toggleClass('arrow_active');
-	});	
+	var contentColumnMaxHeight = 0;
+	
+	$('.conent-column').each(function() {
+		if(contentColumnMaxHeight < $(this).outerHeight()) {
+			contentColumnMaxHeight = $(this).outerHeight();
+		}
+	});
+
+	$('.conent-column').each(function() {
+		var topPadding = parseInt($(this).css('padding-top'));
+		var bottomPadding = parseInt($(this).css('padding-top'));
+		$(this).height(contentColumnMaxHeight - (topPadding + bottomPadding));
+	});
+
+	$('#mainHeader a').each(function() {
+		$(this).hover(function() {
+			var element = $(this).parent().children('.arrow');
+
+			element.toggleClass('arrow');
+			element.toggleClass('arrow_active');
+		}, function () {
+			var element = $(this).parent().children('.arrow_active');
+
+			element.toggleClass('arrow');
+			element.toggleClass('arrow_active');
+		});	
+	});
 });
